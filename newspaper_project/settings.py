2b60c98fc4b7e4e0cc38a74d03c445a2ad10cfe0
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2020-07-01 20:20:34 +0200

    Use custom user model

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -80,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
