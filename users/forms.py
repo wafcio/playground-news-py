5d9bc8add4c4e4c0aedc4a6f2fc6e079bb38fef2
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2020-07-06 22:54:41 +0200

    Add email field in forms

--- users/forms.py
+++ users/forms.py
@@ -6,9 +6,9 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email', 'age',)
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm):
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email', 'age',)
