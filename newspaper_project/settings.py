1c39eb22cd09add31650855dd8a5652d8bb5b277
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2020-07-08 00:05:55 +0200

    Use SendGrind for sending emails

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -131,4 +131,12 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+DEFAULT_FROM_EMAIL = 'yout_custom_email_account'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = 'sendgrid_password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
